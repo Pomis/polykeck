f6b1bfe4d24e1a7b129a87c3fc0c582feecbc6be
Styort <styort02@gmail.com>
2017-03-16 18:33:39 +0300

    auth activity

--- app/src/main/java/pomis/app/wineglass/AuthActivity.java
+++ app/src/main/java/pomis/app/wineglass/AuthActivity.java
@@ -1,13 +1,27 @@
 package pomis.app.wineglass;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dd.processbutton.iml.ActionProcessButton;
+
+import butterknife.BindView;
 
 public class AuthActivity extends AppCompatActivity {
+    @BindView(R.id.vk_auth_butt) ActionProcessButton vkButt;
+    @BindView(R.id.fb_auth_butt) ActionProcessButton fbButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        //ButterKnife.bind(this);
+
     }
+
+    /*
+    @OnClick(R.id.vk_auth_butt) void auth(){
+        vkButt.setProgress(50);
+    }*/
 }
