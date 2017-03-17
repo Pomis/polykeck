d35ab29182aa06e2740cf0b451e3cfd15203b176
Styort <styort02@gmail.com>
2017-03-17 12:19:19 +0300

    view pager add

--- app/src/main/java/pomis/app/wineglass/PersonInfoActivity.java
+++ app/src/main/java/pomis/app/wineglass/PersonInfoActivity.java
@@ -1,10 +1,12 @@
 package pomis.app.wineglass;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -46,6 +48,14 @@ public class PersonInfoActivity extends AppCompatActivity {
         backPhoto.setColorFilter(new LightingColorFilter(0xFF7F7F7F, 0x00000000)); //затемняем фото
         rlCocktail.setAlpha(0.2f);
         rlWine.setAlpha(0.2f);
+
+        profilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PersonInfoActivity.this, PhotoViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/pomis/app/wineglass/PhotoViewActivity.java
@@ -0,0 +1,73 @@
+package pomis.app.wineglass;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PhotoViewActivity extends AppCompatActivity {
+    @BindView(R.id.vp_profile_images) ViewPager vpProfilePhoto;
+    static final int NUM_ITEMS = 3;
+    ImageFragmentPagerAdapter imageFragmentPagerAdapter;
+    public static final String[] IMAGE_NAME = {"profile_photo", "profile_photo1", "profile_photo2"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_view);
+
+        ButterKnife.bind(this);
+
+        imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(getSupportFragmentManager());
+        vpProfilePhoto.setAdapter(imageFragmentPagerAdapter);
+    }
+
+    public static class ImageFragmentPagerAdapter extends FragmentPagerAdapter {
+        public ImageFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            SwipeFragment fragment = new SwipeFragment();
+            return SwipeFragment.newInstance(position);
+        }
+    }
+
+    public static class SwipeFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View swipeView = inflater.inflate(R.layout.swipe_item, container, false);
+            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView);
+            Bundle bundle = getArguments();
+            int position = bundle.getInt("position");
+            String imageFileName = IMAGE_NAME[position];
+            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "pomis.app.wineglass");
+            imageView.setImageResource(imgResId);
+            return swipeView;
+        }
+
+        static SwipeFragment newInstance(int position) {
+            SwipeFragment swipeFragment = new SwipeFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            swipeFragment.setArguments(bundle);
+            return swipeFragment;
+        }
+    }
+}
