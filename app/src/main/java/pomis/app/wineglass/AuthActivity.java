409201cc51978fa396c55500f0d80ea92a4eab5d
Styort <styort02@gmail.com>
2017-03-17 15:00:24 +0300

    add auth

--- app/src/main/java/pomis/app/wineglass/AuthActivity.java
+++ app/src/main/java/pomis/app/wineglass/AuthActivity.java
@@ -4,36 +4,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.dd.processbutton.FlatButton;
+import com.dd.processbutton.iml.ActionProcessButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity implements ProgressGenerator.OnCompleteListener {
     @BindView(R.id.vk_auth_butt)
-    FlatButton vkButt;
+    ActionProcessButton vkButt;
 
     @BindView(R.id.fb_auth_butt)
-    FlatButton fbButt;
+    ActionProcessButton fbButt;
+    private ProgressGenerator progressGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         ButterKnife.bind(this);
-
+        progressGenerator =new ProgressGenerator(this);
     }
 
 
     @OnClick(R.id.vk_auth_butt) void authVk() {
-        Intent intent = new Intent(AuthActivity.this, SearchActivity.class);
-        startActivity(intent);
-        finish();
+        progressGenerator.start(vkButt);
     }
     @OnClick(R.id.fb_auth_butt) void authFb() {
-        Intent intent = new Intent(AuthActivity.this, PersonInfoActivity.class);
-        startActivity(intent);
-        finish();
+        progressGenerator.start(fbButt);
+    }
+
+    @Override
+    public void onComplete() {
+        if(vkButt.getProgress()==100){
+            Intent intent = new Intent(AuthActivity.this, SearchActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if(fbButt.getProgress()==100){
+            Intent intent = new Intent(AuthActivity.this, SearchActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
